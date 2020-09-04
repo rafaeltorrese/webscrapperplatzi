0d2838417fbf96c87a36a91cf326112e65ad4922
Rafael Torres Escobar <torresc.rafael@gmail.com>
2020-09-04 16:44:08 -0500

    NewsPage SuperClass

--- news_page_object.py
+++ news_page_object.py
@@ -2,23 +2,12 @@ import requests
 import bs4
 from common import config
 
-
-
-class HomePage:
+class NewsPage:
     def __init__(self, news_site_uid, url):
         self._config = config()["news_site"][news_site_uid]
         self._queries = self._config['queries']
         self._html = None
         self._visit(url)  # initialize private variable
-        
-    @property
-    def article_links(self):
-        link_list = []
-        for link in self._select(self._queries["homepage_article_links"]):
-            if link and link.has_attr("href"):
-                link_list.append(link)
-        return set(link["href"] for link in link_list)  # elimante duplicates
-            
     
     def _select(self, query_string):
         return self._html.select(query_string)
@@ -29,4 +18,21 @@ class HomePage:
         response.raise_for_status()
         
         self._html = bs4.BeautifulSoup(response.text, "html.parser") # initialize html
+
+class HomePage(NewsPage):
+    def __init__(self, news_site_uid, url):
+        super().__init__(news_site_uid, url)
+        
+    @property
+    def article_links(self):
+        link_list = []
+        for link in self._select(self._queries["homepage_article_links"]):
+            if link and link.has_attr("href"):
+                link_list.append(link)
+        return set(link["href"] for link in link_list)  # elimante duplicates
+            
+class ArticlePage(NewsPage):
+    def __init__(self, news_site_uid, url):
+        super().__init__(news_site_uid, url)
+    
         
